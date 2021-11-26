52aaab01e8bc5b1b3aba8fbfa52e0ba8baa7a451
Sam Lu <549429518@qq.com>
2021-11-26 17:27:03 +0800

    temp20211126

--- src/SamLu.StateMachine/ObjectModel/InputSymbols.cs
+++ src/SamLu.StateMachine/ObjectModel/InputSymbols.cs
@@ -8,9 +8,9 @@ using System.Threading.Tasks;
 
 namespace SamLu.StateMachine.ObjectModel
 {
-    public abstract class InputSymbols<T> : IInputSymbols<T>
+    public abstract class InputSymbols<T> : IInputSymbols<T?>
     {
-        protected LinkedList<T> edges = new();
+        protected LinkedList<T?> edges = new();
 
         public virtual int Count => this.Count();
         public virtual bool IsSynchronized => false;
@@ -44,11 +44,11 @@ namespace SamLu.StateMachine.ObjectModel
             switch (other)
             {
                 case null: throw new ArgumentNullException(nameof(other));
-                case IEnumerable<T>:
-                    this.ExceptWith((IEnumerable<T>)other);
+                case IEnumerable<T?>:
+                    this.ExceptWith((IEnumerable<T?>)other);
                     break;
                 default:
-                    this.ExceptWith(other.Cast<T>());
+                    this.ExceptWith(other.Cast<T?>());
                     break;
             }
         }
@@ -67,11 +67,11 @@ namespace SamLu.StateMachine.ObjectModel
             switch (other)
             {
                 case null: throw new ArgumentNullException(nameof(other));
-                case IEnumerable<T>:
-                    this.IntersectWith((IEnumerable<T>)other);
+                case IEnumerable<T?>:
+                    this.IntersectWith((IEnumerable<T?>)other);
                     break;
                 default:
-                    this.IntersectWith(other.Cast<T>());
+                    this.IntersectWith(other.Cast<T?>());
                     break;
             }
         }
@@ -91,11 +91,11 @@ namespace SamLu.StateMachine.ObjectModel
             switch (other)
             {
                 case null: throw new ArgumentNullException(nameof(other));
-                case IEnumerable<T>:
-                    this.SymmetricExceptWith((IEnumerable<T>)other);
+                case IEnumerable<T?>:
+                    this.SymmetricExceptWith((IEnumerable<T?>)other);
                     break;
                 default:
-                    this.SymmetricExceptWith(other.Cast<T>());
+                    this.SymmetricExceptWith(other.Cast<T?>());
                     break;
             }
         }
@@ -112,11 +112,11 @@ namespace SamLu.StateMachine.ObjectModel
             switch (other)
             {
                 case null: throw new ArgumentNullException(nameof(other));
-                case IEnumerable<T>:
-                    this.UnionWith((IEnumerable<T>)other);
+                case IEnumerable<T?>:
+                    this.UnionWith((IEnumerable<T?>)other);
                     break;
                 default:
-                    this.UnionWith(other.Cast<T>());
+                    this.UnionWith(other.Cast<T?>());
                     break;
             }
         }
@@ -126,8 +126,8 @@ namespace SamLu.StateMachine.ObjectModel
             other switch
             {
                 null => throw new ArgumentNullException(nameof(other)),
-                IEnumerable<T> => this.IsProperSubsetOf((IEnumerable<T>)other),
-                _ => this.IsProperSubsetOf(other.Cast<T>())
+                IEnumerable<T?> => this.IsProperSubsetOf((IEnumerable<T?>)other),
+                _ => this.IsProperSubsetOf(other.Cast<T?>())
             };
 
         public virtual bool IsProperSupersetOf(IEnumerable<T?> other) => this.Count > other.Count() && this.IsSupersetOf(other);
@@ -135,8 +135,8 @@ namespace SamLu.StateMachine.ObjectModel
             other switch
             {
                 null => throw new ArgumentNullException(nameof(other)),
-                IEnumerable<T> => this.IsProperSupersetOf((IEnumerable<T>)other),
-                _ => this.IsProperSupersetOf(other.Cast<T>())
+                IEnumerable<T?> => this.IsProperSupersetOf((IEnumerable<T?>)other),
+                _ => this.IsProperSupersetOf(other.Cast<T?>())
             };
 
         public virtual bool IsSubsetOf(IEnumerable<T?> other) => this.All(item => (other ?? throw new ArgumentNullException(nameof(other))).Contains(item));
@@ -144,8 +144,8 @@ namespace SamLu.StateMachine.ObjectModel
             other switch
             {
                 null => throw new ArgumentNullException(nameof(other)),
-                IEnumerable<T> => this.IsSubsetOf((IEnumerable<T>)other),
-                _ => this.IsSubsetOf(other.Cast<T>())
+                IEnumerable<T?> => this.IsSubsetOf((IEnumerable<T?>)other),
+                _ => this.IsSubsetOf(other.Cast<T?>())
             };
 
         public virtual bool IsSupersetOf(IEnumerable<T?> other) => (other ?? throw new ArgumentNullException(nameof(other))).All(item => this.Contains(item));
@@ -153,8 +153,8 @@ namespace SamLu.StateMachine.ObjectModel
             other switch
             {
                 null => throw new ArgumentNullException(nameof(other)),
-                IEnumerable<T> => this.IsSupersetOf((IEnumerable<T>)other),
-                _ => this.IsSupersetOf(other.Cast<T>())
+                IEnumerable<T?> => this.IsSupersetOf((IEnumerable<T?>)other),
+                _ => this.IsSupersetOf(other.Cast<T?>())
             };
 
         public virtual bool Overlaps(IEnumerable<T?> other) => this.Intersect(other ?? throw new ArgumentNullException(nameof(other)), this).Any();
@@ -162,8 +162,8 @@ namespace SamLu.StateMachine.ObjectModel
             other switch
             {
                 null => throw new ArgumentNullException(nameof(other)),
-                IEnumerable<T> => this.Overlaps((IEnumerable<T>)other),
-                _ => this.Overlaps(other.Cast<T>())
+                IEnumerable<T?> => this.Overlaps((IEnumerable<T?>)other),
+                _ => this.Overlaps(other.Cast<T?>())
             };
 
         public virtual bool SetEquals(IEnumerable<T?> other) => this.SequenceEqual(other ?? throw new ArgumentNullException(nameof(other)), this);
@@ -172,21 +172,21 @@ namespace SamLu.StateMachine.ObjectModel
             other switch
             {
                 IInputSymbols => this.SetEquals((IInputSymbols)other),
-                IEnumerable<T> => this.SetEquals((IEnumerable<T>)other),
-                _ => this.SetEquals(other.Cast<T>())
+                IEnumerable<T?> => this.SetEquals((IEnumerable<T?>)other),
+                _ => this.SetEquals(other.Cast<T?>())
             };
 
 
-        public virtual int Compare(T? x, T? y) => Comparer<T>.Default.Compare(x, y);
+        public virtual int Compare(T? x, T? y) => Comparer<T?>.Default.Compare(x, y);
         public virtual int Compare(object? x, object? y) => this.Compare((T?)x, (T?)y);
 
-        public virtual bool Equals(T? x, T? y) => EqualityComparer<T>.Default.Equals(x, y);
+        public virtual bool Equals(T? x, T? y) => EqualityComparer<T?>.Default.Equals(x, y);
         public virtual new bool Equals(object? x, object? y) => this.Equals((T?)x, (T?)y);
 
-        public virtual int GetHashCode(T? obj) => obj is null ? 0 : EqualityComparer<T>.Default.GetHashCode(obj);
+        public virtual int GetHashCode(T? obj) => obj is null ? 0 : EqualityComparer<T?>.Default.GetHashCode(obj);
         public virtual int GetHashCode([AllowNull] object obj) => this.GetHashCode((T?)obj);
 
-        protected virtual IEnumerable<T?> EnumerateInternal(IEnumerable<T> edges)
+        protected virtual IEnumerable<T?> EnumerateInternal(IEnumerable<T?> edges)
         {
             var ranges = edges.Chunk(2);
             foreach (var range in ranges)
@@ -251,7 +251,74 @@ namespace SamLu.StateMachine.ObjectModel
             if (this.Compare(from, to) > 0) (from, to) = (to, from); // 调整为 (最小值, 最大值) 形式。
 
             var (fromExists, fromFrom, fromTo) = Find(from);
-            var (toExists, toFrom, toTo) = Find(to);
+            if (!fromExists && fromTo is null) // 若查询到的下界的位置不存在且在末尾。
+            {
+                this.edges.AddLast(from);
+                this.edges.AddLast(to);
+                return true;
+            }
+            var (toExists, toFrom, toTo) = Find(to, startNode: fromFrom);
+            if (!toExists && toFrom is null) // 若查询到的上界的位置不存在且在开头。
+            {
+                this.edges.AddFirst(to);
+                this.edges.AddLast(from);
+                return true;
+            }
+
+            // 此时， [from, to] 范围必定不会——在末尾或开头且不与输入符号集有共同元素。
+            LinkedListNode<T?> deleteFrom, deleteTo;
+            if (fromExists) // 下界位置位于已有范围内，则不需要插入边界，从位置所在范围的上界开始删除。
+            {
+                deleteFrom = fromTo!;
+            }
+            else if (fromFrom is null) // 下届位置位于开头，则需要插入边界，并从第一个范围的下界开始删除。
+            {
+                deleteFrom = this.edges.First!;
+                this.edges.AddFirst(from);
+            }
+            else if (this.NextTo(from, fromFrom.Value)) // 下界位置紧邻前方范围的上界，则不需要插入边界，从前方范围的上界开始删除。
+            {
+                deleteFrom = fromFrom;
+            }
+            else if (this.NextTo(from, fromTo!.Value)) // 下界位置紧邻后方范围的下界，则需要修改后方范围的下界，从后方范围的上界开始删除。
+            {
+                deleteFrom = fromTo!.Next!;
+                fromTo!.Value = from;
+            }
+            else // 下界位置不紧邻前、后方范围的边界，则需要插入边界，并从后方范围的下界开始删除。
+            {
+                deleteFrom = fromTo;
+                this.edges.AddAfter(fromTo, from);
+            }
+            if (toExists) // 上界位置位于已有范围内，则不需要插入边界，从位置所在范围的下界开始删除。
+            {
+                deleteTo = toFrom!;
+            }
+            else if (toTo is null) // 上界位置位于结尾，则需要插入边界，并从最后一个范围的上界开始删除。
+            {
+                deleteTo = this.edges.Last!;
+                this.edges.AddLast(to);
+            }
+            else if (this.NextTo(to, toTo.Value)) // 上界位置紧邻后方范围的下界，则不需要插入边界，从后方范围的下界开始删除。
+            {
+                deleteTo = toTo;
+            }
+            else if (this.NextTo(to, toFrom!.Value)) // 上界位置紧邻前方范围的上界，则需要修改前方范围的上界，从前方范围的下界开始删除。
+            {
+                deleteTo = toFrom!.Next!;
+                toFrom!.Value = to;
+            }
+            else // 上界位置不紧邻前、后方范围的边界，则需要插入边界，并从前方范围的上界开始删除。
+            {
+                deleteTo = toFrom;
+                this.edges.AddBefore(toFrom, to);
+            }
+
+            // 删除失效的边界。
+            if (this.Compare(deleteFrom.Value, deleteTo.Value) > 0) return false; // 没有失效的边界。
+            for (var node = deleteFrom; node != deleteTo; node = node.Next!)
+                this.edges.Remove(node);
+            this.edges.Remove(deleteTo);
         }
         public virtual bool Add(object? from, object? to) => this.Add((T?)from, (T?)to);
 
@@ -299,7 +366,7 @@ namespace SamLu.StateMachine.ObjectModel
         public virtual bool Remove(T? from, T? to);
         public virtual bool Remove(object? from, object? to) => this.Remove((T?)from, (T?)to);
 
-        protected (bool exists, LinkedListNode<T>? from, LinkedListNode<T>? to) Find(T? item, LinkedListNode<T>? startNode = null, LinkedListNode<T>? endNode = null)
+        protected (bool exists, LinkedListNode<T?>? from, LinkedListNode<T?>? to) Find(T? item, LinkedListNode<T?>? startNode = null, LinkedListNode<T?>? endNode = null)
         {
             startNode ??= this.edges.First;
             endNode ??= this.edges.Last;
@@ -308,7 +375,7 @@ namespace SamLu.StateMachine.ObjectModel
             else if (this.Compare(item, startNode) < 0) return (false, null, startNode);
             else if (this.Compare(item, endNode) > 0) return (false, endNode, null);
 
-            LinkedListNode<T>? pos = null; // 一个定位，指向上一次检测的范围的上界。
+            LinkedListNode<T?>? pos = null; // 一个定位，指向上一次检测的范围的上界。
             do
             {
                 // 成对获取范围的上下界。
