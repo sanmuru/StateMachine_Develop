61366a1182ce7f2c2eceefefbb56be68fd28fda9
Sam Lu <549429518@qq.com>
2024-12-04 10:47:14 +0800

    Update CharTests.cs

--- src/RegexTest/CharTests.cs
+++ src/RegexTest/CharTests.cs
@@ -1,3 +1,5 @@
+using System.Collections.Specialized;
+
 namespace SamLu.RegularExpression.UnitTests;
 
 public class CharTests
@@ -6,12 +8,39 @@ public class CharTests
 
     private RegexConst<char> Const(char c) => new(this.provider, c);
 
+    private RegexRange<char> Range(char start, char end) => new(this.provider, start, end);
+
     [Fact]
-    public void ConstTest()
+    public void ConstTest_ASCII()
     {
-        for (char c = char.MinValue; c <= char.MaxValue; c++)
+        for (int i = char.MinValue; i <= 0x7F; i++)
         {
+            var c = (char)i;
             Assert.True(Const(c).IsMatch(c));
+        }
+    }
+
+    [Fact]
+    public void ConstTest_UNICODE()
+    {
+        for (int i = char.MinValue; i <= char.MaxValue; i++)
+        {
+            var c = (char)i;
+            Assert.True(Const(c).IsMatch(c));
+        }
+    }
+
+    [Fact]
+    public void RangeTest_ASCII()
+    {
+        var range = Range((char)0x00, (char)0x7F);
+        for (int i = char.MinValue; i <= char.MaxValue; i++)
+        {
+            char c = (char)i;
+            if (i is >= 0x00 and <= 0x7F)
+                Assert.True(range.IsMatch(c));
+            else
+                Assert.False(range.IsMatch(c));
         }
     }
 }
